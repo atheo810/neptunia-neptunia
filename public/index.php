a4a7b8a93330ff1bda0555df16a62ed22695b2d7
atheo810 <atheos810@gmail.com>
2023-01-06 23:30:38 +0800

    nesa serve itu jalan ke public/index.php

--- public/index.php
+++ public/index.php
@@ -1,1 +1,16 @@
-<?php 
+<!-- <pre>
+<?php var_dump($_SERVER); ?>
+</pre>
+ -->
+
+ <?php
+
+require "vendor/autoload.php";
+
+use Neptunia\Config\Database\UserDatabase;
+
+$db = new UserDatabase();
+
+var_dump($db);
+
+?>
