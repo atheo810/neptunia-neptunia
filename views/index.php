49ec4d75c1a342236a48f07d7785323c05b0a839
atheo810 <atheos810@gmail.com>
2023-01-07 12:39:12 +0800

    fixing index.php

--- views/index.php
+++ views/index.php
@@ -3,9 +3,9 @@
 require "../vendor/autoload.php";
 use Neptunia\Config\Database\Queries\Query;
 
-// $db = new Query();
-// $db->selectAll("coba");
-// foreach ($db->selectAll("coba") as $data) {
-//     print_r($data);
-//     echo "<br>";
-// }
+$db = new Query();
+$db->selectAll("coba");
+foreach ($db->selectAll("daftar_supir") as $data) {
+    print_r($data['nama']);
+    echo "<br>";
+}
