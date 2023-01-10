99bedfceaf01898564af851a82c91664c1d4f7ad
atheo810 <atheos810@gmail.com>
2023-01-10 19:33:25 +0800

    penghapusan symfony dari framework

--- routes/web.php
+++ routes/web.php
@@ -1,1 +1,48 @@
 <?php 
+/**
+ * 
+ * 2 parameter 
+ * route : alamat routing
+ * file : lokasi file yang dituju jika alamat route sama
+ */
+class web
+{
+	
+	function __construct(argument)
+	{
+	}
+	public function add($route, $file)
+	{
+		//mengganti garing di depan dan belakang
+		//$_REQUEST['uri'] akan kosong jika /
+
+        if(!empty($_REQUEST['uri'])){
+            $route = preg_replace("/(^\/)|(\/$)/","",$route);
+            $reqUri =  preg_replace("/(^\/)|(\/$)/","",$_REQUEST['uri']);
+        }else{
+            $reqUri = "/";
+        }
+
+        if($reqUri == $route ){
+
+            //jika ditemukan maka akan include file
+            include($file);
+
+            //keluar jika alamat route ditemukan.
+            exit();
+        }
+	}
+	public function notfound($file)
+	{
+	  include($file);
+	}
+}
+
+// Route instance
+$route = new Web();
+
+// alamat route dan lokasi file
+$route->add("/home", "index.php");
+$route->notfound("404.php");
+
+
