c7191af9e069e464bc91524f74988b0445a70be0
atheo810 <atheos810@gmail.com>
2023-01-12 20:20:49 +0800

    merubah class Web menjadi web

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,10 @@
 use Neptunia\Config\Routes\web;
 
 // Route instance
-$route = new Web();
+$route = new web();
 
 // alamat route dan lokasi file
-$route->add("/home/", "home.php");
-$route->add("/home/{id}", "home.php");
+$route->add("/home", "home.php");
+$route->add("/home/{baru}", "home.php");
 $route->add("/", "index.php");
 $route->notfound("404.php");
