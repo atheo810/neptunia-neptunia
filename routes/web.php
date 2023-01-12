2f62a5c2a88b9dd96dcd3f416c00d7c1c6de9632
atheo810 <atheos810@gmail.com>
2023-01-12 13:11:24 +0800

    ini routing sudah bisa diapakai blm di coba untuk passing parameter.. by atheo

--- routes/web.php
+++ routes/web.php
@@ -1,38 +1,6 @@
 <?php
 
-/**
- *
- * 2 parameter
- * route : alamat routing
- * file : lokasi file yang dituju jika alamat route sama
- */
-class web
-{
-    public function add($route, $file)
-    {
-        //mengganti garing di depan dan belakang
-        //$_SERVER['REQUEST_URI'] akan kosong jika /
-
-        if (!empty($_SERVER['REQUEST_URI'])) {
-            $route = preg_replace("/(^\/)|(\/$)/", "", $route);
-            $reqUri =  preg_replace("/(^\/)|(\/$)/", "", $_SERVER['REQUEST_URI']);
-        } else {
-            $reqUri = "/";
-        }
-
-        if ($reqUri == $route) {
-            //jika ditemukan maka akan include file
-            include("../views/".$file);
-
-            //keluar jika alamat route ditemukan.
-            exit();
-        }
-    }
-    public function notfound($file)
-    {
-        include("../views/".$file);
-    }
-}
+use Neptunia\Config\Routes\web;
 
 // Route instance
 $route = new Web();
