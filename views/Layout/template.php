ddba4665203e673a389eaa49b8262b7ee5efd4e2
atheo810 <atheos810@gmail.com>
2023-01-22 11:24:57 +0800

    perubahaan struktur folder dan penggunaan layouting

--- views/Layout/template.php
+++ views/Layout/template.php
@@ -1,8 +1,8 @@
 
-<?php include_once __DIR__ . "/../Component/header.php"; ?>
-<?php include_once __DIR__ . "/../Component/navbar.php"; ?>
+<?php include_once __DIR__ . "/header.php"; ?>
+<?php include_once __DIR__ . "/navbar.php"; ?>
 
 <div class="container">
 	{{content}}
 </div>
-<?php include_once __DIR__ . "/../Component/footer.php"; ?>
+<?php include_once __DIR__ . "/footer.php"; ?>
