4e7888a5bdafc55fd4966f6b95cb6a6a60637b7b
atheo810 <atheos810@gmail.com>
2023-01-30 16:02:42 +0800

    penggunaan tailwind

---
+++ public/src/js/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./../../../views/**/*.{html,php}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
---
+++ views/Component/navbar.php
@@ -0,0 +1,21 @@
+<nav class="bg-gray">
+    <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div class="relative flex h-16 items-center justify-between">
+            <div class="absolute inset-y-0 left-0 items-center sm:hidden">
+                <!-- Mobile menu button -->
+                <button type="button" class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-white" aria-controls="mobile-menu" aria-expanded="false"">
+                    <span class=" sr-only">Open main menu</span>
+                    <!-- Icon when menu is closed. Heroicon name: outline/bars-3 Menu open: "hidden", Menu closed: "block"-->
+                    <svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                    </svg>
+                    <svg class="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </button>
+            </div>
+            <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                <div class=""></div>
+            </div>
+        </div>
+</nav>
